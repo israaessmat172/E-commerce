2978dfa052544d168327e9f106ac9aaa0b4d0b5e
israa <israa@gmail.com>
2024-05-20 06:26:29 +0300

    preparing the API server

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ const db = require("./db");
 const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const productsRouter = require("./routers/products");
+const cors = require("cors");
+require("dotenv/config");
+
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
-require("dotenv/config");
+
+const productsRouter = require("./routes/products");
+const usersRouter = require("./routes/users");
+const categoriesRouter = require("./routes/categories");
+const ordersRouter = require("./routes/orders");
 
 const api = process.env.API_URL;
 
 app.use(`${api}/products`, productsRouter);
+app.use(`${api}/users`, usersRouter);
+app.use(`${api}/orders`, ordersRouter);
+app.use(`${api}/categories`, categoriesRouter);
 
 app.listen(5000, () => {
   console.log(api);
